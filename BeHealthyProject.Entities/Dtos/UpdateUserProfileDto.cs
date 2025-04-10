c705a1402435b1d832e7a4fad78049cc509fb960
Ahmed_ud72 <Ahmed_ud72@STHQ0118-09>
2025-04-10 17:37:26 +0400

    Updated check isComplete

--- BeHealthyProject.Entities/Dtos/UpdateUserProfileDto.cs
+++ BeHealthyProject.Entities/Dtos/UpdateUserProfileDto.cs
@@ -1,10 +1,12 @@
 ﻿namespace BeHealthyProject.Server.Dtos
 {
-	public class UpdateUserProfileDto
-	{
-		public string? Nickname { get; set; }
-		public int? Age { get; set; }
-		public double? Height { get; set; }
-		public double? Weight { get; set; }
-	}
+    public class UpdateUserProfileDto
+    {
+        public string? Nickname { get; set; }
+        public int? Age { get; set; }
+        public double? Height { get; set; }
+        public double? Weight { get; set; }
+
+        public bool IsCompleteProfile { get; set; }
+    }
 }
