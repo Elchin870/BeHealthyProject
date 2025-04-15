9c67c5d5ca809bf9207b8e359cbf891954bb925c
Ahmedov001 <ahmedovferid666@gmail.com>
2025-04-15 14:15:20 +0400

    401 Page image

--- behealthyproject.client/src/components/Forbidden.jsx
+++ behealthyproject.client/src/components/Forbidden.jsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Forbidden = () => (
-    <div className="container text-center mt-5">
+    <div className="container text-center mt-5 homepage">
         <div className="card shadow p-4">
             <h2 className="text-danger">403 - Forbidden</h2>
             <p className="text-muted">You do not have permission to view this page.</p>
--- behealthyproject.client/src/components/Unauthorized.jsx
+++ behealthyproject.client/src/components/Unauthorized.jsx
@@ -1,11 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Unauthorized = () => (
-    <div className="container text-center mt-5">
-        <div className="card shadow p-4">
-            <h2 className="text-warning">401 - Unauthorized</h2>
-            <p className="text-muted">You must log in to view this page.</p>
-            <a href="/" className="btn btn-outline-success mt-3">Login</a>
+    <div className="unauthorizeimg">
+        <div className="container text-center mt-5">
+            <div className="mt-5">
+                <p className="text-white" style={
+                    {
+                        marginTop: "12em",
+                        marginLeft:"1em",
+                        fontSize:"23px"
+                    }
+                }>Unauthorized - You must log in to view this page.</p>
+                <a href="/" className="btn btn-success ">Login</a>
+            </div>
         </div>
     </div>
 );
